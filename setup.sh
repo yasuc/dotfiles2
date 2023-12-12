98622f37496289113010577223a3169cda872201
IHA Yasushi <yasuc18@gmail.com>
2023-12-13 01:02:25 +0900

    feat: 🎸 add sheldon config directory

--- setup.sh
+++ setup.sh
@@ -2,17 +2,14 @@
 
 DOT_FILES=(.byobu .dir_colors .gdbinit .gitconfig .gitignore .inputrc .p10k.zsh .screenrc .tmux.conf .zsh .zshrc)
 
-for file in ${DOT_FILES[@]}
-do
-    rm $HOME/$file
-    ln -s $PWD/$file $HOME/$file
+for file in ${DOT_FILES[@]}; do
+	rm $HOME/$file
+	ln -s $PWD/$file $HOME/$file
 done
 
-CONFIG_DIRS=(nvim)
+CONFIG_DIRS=(nvim tmux sheldon)
 
-for dir in ${CONFIG_DIRS[@]}
-do
-    rm -rf $HOME/.config/$dir
-    ln -s $PWD/$dir $HOME/.config/$dir
+for dir in ${CONFIG_DIRS[@]}; do
+	rm -rf $HOME/.config/$dir
+	ln -s $PWD/$dir $HOME/.config/$dir
 done
-
